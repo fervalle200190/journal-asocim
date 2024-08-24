c7c1c4671ad2e967dd5b64752c9d9b78513980a8
Fernando Vallejo <fernando.vallejo@longevo.com>
2024-08-24 06:18:21 -0400

    changed word

--- components/weekly-journal/eventActivityList.ts
+++ components/weekly-journal/eventActivityList.ts
@@ -3,6 +3,6 @@ import { WeeklyActivityList } from "./WeeklyJournalList"
 export const eventActivityList: WeeklyActivityList = [
      {
           weekDayName: "Merida's trip",
-          activities: `On April this year, I went on a really amazing trip, it was a trip to Merida, a state of Venezuela, I got on the bus at about 10:00PM, in the next day, I arrived at 6:30AM to the Merida's Paramos, We wandered around the Mucubaji lake, it was such a beautiful view, and then We arrived to the Apartaderos town, where we walked trough the town, finally we arrived to Merida's city, the second day we went to the Pico espejo, it was amazing, and at night we went to a night club, then on the third day, we went to the zoo, Pico el Aguila and Victoria's lake, at 3:00PM we returned home, it was a really amazing trip.`,
+          activities: `On April this year, I went on a really amazing trip, it was a trip to Merida, a state of Venezuela, I got on the bus at about 10:00PM, in the next day, I arrived at 6:30AM to the Merida's Paramos, We wandered around the Mucubaji lake, it was such a beautiful view, and then We arrived to the Apartaderos town, where we walked trough the town, finally we arrived to Merida's city, the second day we went to the Pico espejo, it was amazing, and at night we went to a night club, then on the third day, we went to the zoo, Pico el Aguila and Victoria's lake, at 3:00PM we returned home, it was a really good trip.`,
      },
 ]
